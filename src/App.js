f96bcafffabf323d5d58c185072a04643e192052
iamds <shaun@shaun-pillid.com>
2023-01-10 20:32:50 +0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [value, setValue]= useState("");
+  const handleClick = (e) =>{
+    setValue(value+""+e.target.value);
+  };
+  const calculatorExp = /^[+-]?([0-9]*[.])?[0-9]+([+ * \/ -]([0-9]*[.])?[0-9]+)*$/;
+  const handleEqual =()=>{
+    { calculatorExp.test(value) ?  setValue(eval(value)) : alert("Error \"Wrong Expression\"") }
+  };
+
+  const clearFunc =()=>{
+    setValue("");
+  }
+
+  const handleDel = () => {
+    setValue(value.substr(0, value.length - 1));
+  }
+  return (<>
+        <h1><strong>Calculator</strong></h1>
+        <div className="calculator">
+          <input type="text" name="calculatorScreen" placeholder="0" value={value} id="calculatorScreen"/>
+          <button value="9" onClick={handleClick} className="button">9</button>
+          <button value="8" onClick={handleClick} className="button">8</button>
+          <button value="7" onClick={handleClick} className="button">7</button>
+          <button value="6" onClick={handleClick} className="button">6</button>
+          <button value="5" onClick={handleClick} className="button">5</button>
+          <button value="4" onClick={handleClick} className="button">4</button>
+          <button value="3" onClick={handleClick} className="button">3</button>
+          <button value="2" onClick={handleClick} className="button">2</button>
+          <button value="1" onClick={handleClick} className="button">1</button>
+          <button value="0" onClick={handleClick} className="button">0</button>
+
+
+          <button value="." onClick={handleClick} className="button">.</button>
+          <button value="-" onClick={handleClick} className="button">-</button>
+          <button value="+" onClick={handleClick} className="button">+</button>
+          <button value="*" onClick={handleClick} className="button">*</button>
+          <button value="/" onClick={handleClick} className="button">/</button>
+          <button value="del" onClick={handleDel} className="button">del</button>
+          <button value="=" onClick={handleEqual} className="buttonL">=</button>
+          <button value="clear" onClick={clearFunc} className="buttonL">clear</button>
+        </div>
+
+    </>
   );
 }
 
